acbf3f1ed04bbdc71260bb0dcd9afbd3a84a26b1
sushilnitp <77502277+sushilnitp@users.noreply.github.com>
2021-02-27 10:29:42 +0530

    Revert "commit change"
    This reverts commit 3943aed45f6f5026920affe917cc6468c978f32c.

--- run_docker.sh
+++ run_docker.sh
@@ -2,14 +2,11 @@
 
 ## Complete the following steps to get Docker running locally
 
-
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t myimage:1.0 .
 
 # Step 2: 
 # List docker images
-docker image ls
+
 # Step 3: 
 # Run flask app
-docker container run --name web -p 8000:80 myimage:1.0
